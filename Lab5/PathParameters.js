27ecef2326ec642110ec4a56aef5aa7ee9a394ea
eunjinlee23 <eunj.lee8585@gmail.com>
2025-11-17 20:04:01 -0500

    update pathparameters lab

--- Lab5/PathParameters.js
+++ Lab5/PathParameters.js
@@ -6,10 +6,24 @@ export default function PathParameters(app) {
     }
     const substract = (req, res) => {
         const { a, b } = req.params;
-        const sum = parseInt(a) - parseInt(b);
-        res.send(sum.toString());
+        const subtraction = parseInt(a) - parseInt(b);
+        res.send(subtraction.toString());
     }
 
+    const multiply = (req, res) => {
+        const { a, b } = req.params;
+        const multiplies = parseInt(a) * parseInt(b);
+        res.send(multiplies.toString());
+    }
+
+    const divide = (req, res) => {
+        const { a, b } = req.params;
+        const divides = parseInt(a) / parseInt(b);
+        res.send(divides.toString());
+    }
+
+    app.get("/lab5/divide/:a/:b", divide);
+    app.get("/lab5/multiply/:a/:b", multiply);
     app.get("/lab5/add/:a/:b", add);
     app.get("/lab5/subtract/:a/:b", substract);
 }
